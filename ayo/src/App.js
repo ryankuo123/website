63876a8d12213893ce47dc6b15c3dae8869c14a2
ryankuo123 <ryanlkuo@icloud.com>
2023-10-18 19:53:36 -0700

    finish

--- ayo/src/App.js
+++ ayo/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <p className='name'>Ryan Kuo</p>
+      <br/>
+      <div className='text'>
+        <p>ryanlkuo@icloud.com</p>
+        <p>(832) 206-5938</p>
+        <p>ryanlkuo.wixsite.com/artwork</p>
+      </div>
+      <br/>
+      <p className='title'>Education</p>
+      <p className='text'>━━━━━━━━━━━━</p>
+      <div className='text'>
+        <p>-Texas A&M University at College Station, August 2021 - Present</p>
+        <p>-Currently studying Visualization in the School of Performance, Visualization, and Fine arts</p>
+      </div>
+      <br/>
+      <p className='title'>Skills</p>
+      <p className='text'>━━━━━━━━━━━━</p>
+      <div className='text'>
+        <p>-Photoshop</p>
+        <p>-Maya</p>
+        <p>-Unreal Engine 5</p>
+        <p>-Substance Painter 3D</p>
+      </div>
+      <br/>
+      <p className='title'>Languages</p>
+      <p className='text'>━━━━━━━━━━━━</p>
+      <div className='text'>
+        <p>-3 Years of Chinese (Texas A&M)</p>
+        <p>-3 Years of Spanish (Glenda Dawson Highschool)</p>
+      </div>
     </div>
   );
 }
